70aa29bf61897c80bd40901b55abbdac93ebe0b8
00Danish00 <96422709+00Danish00@users.noreply.github.com>
2022-08-09 16:52:27 +0530

    add file via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+// var Arr=[3,29,24,2,25,52,234];
+
+// var newarr= Arr.slice(1,6);
+// console.log(newarr)
+// // console.log(Arr)
+// // for (i=0; i<Arr.length; i++)
+// // {
+// //     console.log(Arr[i]) 
+// // }
+// Arr.forEach(function(Element)
+// {
+// console.log(Element)
+// })
+// let j=0;
+// do{
+//     console.log(Arr[j])
+//     j ++;
+// }
+// while(j<Arr.length);
+// var myname = "Danish iqbal";
+// console.log(myname.length);
+// console.log(myname.indexOf("Danish"));
+// console.log(myname.replace("Danish", "Tanish"));
+// console.log(myname.slice(2,4))
+
+mydate=new Date();
+console.log(mydate);
